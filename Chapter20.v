417127216f38a62dfe3cd6a64ed2dc13dbbb7b83
Sterling Jeppson <sterlingijeppson@gmail.com>
2024-11-07 21:41:56 -0800

    did some work on chapter 20

--- Chapter20.v
+++ Chapter20.v
@@ -17,6 +17,8 @@ Open Scope type_scope.
 Axiom univalence : forall (A B : Type), (A = B) <-> exists (f : A -> B) (g : B -> A),
   (forall x, g (f x) = x) /\ (forall y, f (g y) = y).
 
+Axiom EquivThenEqual: prop_extensionality.
+
 Close Scope type_scope.
 
 Lemma Relation_is_Ensemble : forall A, Relation A = Ensemble (A * A).
@@ -68,13 +70,51 @@ Ltac solve_in_Union :=
   end.
 
 Section section_20_1.
-  Let A := ⦃ 1, 2, 3, 4, 5, 6 ⦄.
-  
-  Let R : Relation ℝ := ⦃ (1,1),(1,2),(1,3),(1,4),(2,3),(2,5),(2,6),(3,5),(4,5),(4,6) ⦄.
+  Open Scope type_scope.
+  Open Scope R_scope.
+
+  Inductive A := one | two | three | four | five | six.
+
+  Notation "1" := one.
+  Notation "2" := two.
+  Notation "3" := three.
+  Notation "4" := four.
+  Notation "5" := five.
+  Notation "6" := six.
+
+  Let R : Relation A := ⦃ (1,1),(1,2),(1,3),(1,4),(2,3),(2,5),(2,6),(3,5),(4,5),(4,6) ⦄.
+
+  Definition S (n : A) : Ensemble A := (fun x => (R n x)).
 
   Lemma lemma_20_1_a : R 1 1.
   Proof.
     apply x_y_In_implies_rel. unfold R. rewrite ens_rel_ens_id. autoset.
+  Qed.
+
+  Lemma lemma_20_1_b : ~ R 2 1.
+  Proof.
+    assert (H1 : (2,1) ∉ R). { unfold R. rewrite ens_rel_ens_id. autoset. } auto.
+  Qed.
+
+  Lemma S1_elements : S 1 = ⦃ 1, 2, 3, 4 ⦄.
+  Proof.
+    unfold S, R. apply functional_extensionality. intros x. destruct x.
+    - apply EquivThenEqual. split; intros. replace (⦃1,2,3,4⦄ 1) with (1 ∈ ⦃1,2,3,4⦄); autoset.
+      apply x_y_In_implies_rel. rewrite ens_rel_ens_id. autoset.
+    - apply EquivThenEqual. split; intros. replace (⦃1,2,3,4⦄ 2) with (2 ∈ ⦃1,2,3,4⦄); autoset.
+      apply x_y_In_implies_rel. rewrite ens_rel_ens_id. autoset.
+    - admit.
+    - admit.
+    - apply EquivThenEqual. split; intros. apply x_y_In_implies_rel in H.
+      assert (~(1, 5) ∈ ⦃(1, 1),(1, 2),(1, 3),(1, 4),(2, 3),(2, 5),(2, 6),(3, 5),(4, 5),(4, 6)⦄). { autoset. } tauto.
+      assert (~5 ∈ ⦃1,2,3,4⦄). { autoset. } tauto.
+    - admit.
+Admitted.
+
+  Lemma lemma_21_1_c : S 1 = ⦃ 1, 2, 3, 4 ⦄.
+  Proof.
+    apply set_equal_def; intros x; split; intros H1.
+    - unfold S in H1. unfold R in H1. 
   Qed.
 
 End section_20_1.
