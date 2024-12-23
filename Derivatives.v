2ab91b1712ad0e36e61ccb21aedabb5c3aadce26
Sterling Jeppson <sterlingijeppson@gmail.com>
2024-12-23 11:29:27 -0800

    Fix limit definition in differentiable_at and improve proof structure in theorem_9_1 for clarity and rigor.

--- Derivatives.v
+++ Derivatives.v
@@ -2,7 +2,7 @@ Require Import Imports Sets Notations Functions Limit Continuity Reals_util.
 Import SetNotations.
 
 Definition differentiable_at (f:R -> R) (a:R) :=
-  exists L, ⟦ lim a ⟧ (fun h => (f (a + h) - f a) / h) = L.
+  exists L, ⟦ lim 0 ⟧ (fun h => (f (a + h) - f a) / h) = L.
 
 Definition differentiable_on (f:R -> R) (A:Ensemble R) :=
   forall a, a ∈ A -> differentiable_at f a.
@@ -23,7 +23,11 @@ Qed.
 Theorem theorem_9_1 : forall f a,
   differentiable_at f a -> continuous_at f a.
 Proof.
-  intros f a [L H1]. apply lemma_9_1. assert (⟦ lim 0 ⟧ (fun h => (f (a + h) - f a) / h * h) = 0) as H2.
-  { replace 0 with (L * 0) at 2 by lra. apply limit_mult. 2 : { apply limit_id. } 
-    intros ε H2. specialize (H1 ε H2) as [δ [H1 H3]]. exists δ. split; auto. intros x H4.
+  intros f a [L H1]. apply lemma_9_1.
+  assert (⟦ lim 0 ⟧ (fun h => (f (a + h) - f a) / h * h) = 0) as H2.
+  { replace 0 with (L * 0) at 2 by lra. apply limit_mult. 2 : { apply limit_id. } auto. }
+  
+  apply limit_mult with (f1 := fun h => (f (a + h) - f a) / h * h) (L1 := 0) in H1.
+  replace (fun h => (f (a + h) - f a) / h * h) with (fun h => (f (a + h) - f a)) in H2.
+  2 : { extensionality h. field. }
 Qed.
