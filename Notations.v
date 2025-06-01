87e4f3dc79b2e0cffb5d255ed0903a3b4849fcb5
Sterling1111 <sterlingijeppson@gmail.com>
2025-05-31 17:06:48 -0700

    Add notation for interval comparison: a <= b < c <= d

--- Notations.v
+++ Notations.v
@@ -22,6 +22,9 @@ Notation "a > b > c > d" := (a > b /\ b > c /\ c > d)
   (at level 70, b at next level, c at next level, d at next level) : R_scope.
 Notation "a = b = c" := (a = b /\ b = c) 
   (at level 70, b at next level, c at next level) : R_scope.
+Notation  "a <= b < c <= d" := (a <= b /\ b < c /\ c <= d)
+  (at level 70, b at next level, c at next level, d at next level) : R_scope.
+
 
 Declare Scope interval_scope.
 Delimit Scope interval_scope with interval.
