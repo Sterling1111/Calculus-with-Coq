fefa7c60f3996da176c611228077de5642fe1ea9
Sterling1111 <sterlingijeppson@gmail.com>
2025-12-17 21:53:56 -0800

    Refactor derivative lemmas in Problem_10_5.v and Problem_10_6.v to simplify assertions and enhance clarity using auto_diff.

--- Calculus/Chapter10/Problem_10_5.v
+++ Calculus/Chapter10/Problem_10_5.v
@@ -4,16 +4,7 @@ Lemma lemma_10_5_i : ∀ f f' x,
   f = (λ x, 1 / x) -> x ≠ 0 -> ⟦ der x ⟧ f = f' -> f (f'(x)) = -x^2.
 Proof.
   intros f f' x H1 H2 H3.
-  assert (H4 : ⟦ der x ⟧ f = (λ x, -1 / x^2)).
-  {
-    set (fn := λ x : ℝ, 1). set (fd := λ x : ℝ, x). set (fn' := λ x : ℝ, 0). set (fd' := λ x : ℝ, 1).
-    assert (H5 : ⟦ der x ⟧ fn = fn') by apply theorem_10_1.
-    assert (H6 : ⟦ der x ⟧ fd = fd') by apply theorem_10_2.
-    pose proof theorem_10_8 fn fn' fd fd' x H5 H6 ltac:(auto) as H7.
-    replace (fn / fd)%f with f in H7. unfold derivative_at in H7. unfold derivative_at.
-    replace ((fd x * fn' x - fn x * fd' x) / (fd x * fd x)) with (-1 / x ^ 2) in H7.
-    2 : { unfold fn, fd, fn', fd'. field; auto. } auto.
-  }
+  assert (H4 : ⟦ der x ⟧ f = (λ x, -1 / x^2)) by (subst; auto_diff).
   rewrite (derivative_of_function_at_x_unique f f' (λ x : ℝ, -1 / x ^ 2) x H3 H4), H1. field; auto.
 Qed.
 
@@ -21,11 +12,7 @@ Lemma lemma_10_5_ii : ∀ f f',
   f = (λ x, x^2) -> ⟦ der ⟧ f = f' -> ∀ x, f (f'(x)) = 4 * x^2.
 Proof.
   intros f f' H1 H2 x.
-  assert (H3 : ⟦ der ⟧ f = (λ x, 2 * x)).
-  {
-    rewrite H1. replace (λ x0 : ℝ, 2 * x0) with (λ x0 : ℝ, 2 * x0 ^ (2 - 1)).
-    2 : { extensionality y. simpl. lra. } apply power_rule'; auto. 
-  }
+  assert (H3 : ⟦ der ⟧ f = (λ x, 2 * x)) by (subst; auto_diff).
   rewrite (derivative_of_function_unique f f' (λ x, 2 * x) H2 H3), H1. field; auto.
 Qed.
 
@@ -33,8 +20,7 @@ Lemma lemma_10_5_iii : ∀ f f',
   f = (λ x, 17) -> ⟦ der ⟧ f = f' -> ∀ x, f (f'(x)) = 17.
 Proof.
   intros f f' H1 H2 x.
-  assert (H3 : ⟦ der ⟧ f = (λ _ : ℝ, 0)).
-  { rewrite H1. apply theorem_10_1. }
+  assert (H3 : ⟦ der ⟧ f = (λ _ : ℝ, 0)) by (subst; auto_diff).
   rewrite (derivative_of_function_unique f f' (λ _ : ℝ, 0) H2 H3), H1. simpl. lra.
 Qed.
 
@@ -42,12 +28,6 @@ Lemma lemma_10_5_iv : ∀ f f',
   f = (λ x, 17 * x) -> ⟦ der ⟧ f = f' -> ∀ x, f (f'(x)) = 289.
 Proof.
   intros f f' H1 H2 x.
-  assert (H3 : ⟦ der ⟧ f = (λ _ : ℝ, 17)).
-  {
-    rewrite H1.
-    replace (λ _ : ℝ, 17) with (λ x : ℝ, 17 * 1).
-    2 : { extensionality y. lra. }
-    apply theorem_10_5' with (f := (λ x, x)) (f' := (λ _ : ℝ, 1)) (c := 17). apply theorem_10_2.
-  }
+  assert (H3 : ⟦ der ⟧ f = (λ _ : ℝ, 17)) by (subst; auto_diff).
   rewrite (derivative_of_function_unique f f' (λ _ : ℝ, 17) H2 H3), H1. field; auto.
 Qed.
--- Calculus/Chapter10/Problem_10_6.v
+++ Calculus/Chapter10/Problem_10_6.v
@@ -1,39 +1,49 @@
-From Lib Require Import Imports Sets Limit Continuity Derivative Notations Reals_util.
-Import LimitNotations IntervalNotations SetNotations DerivativeNotations Function_Notations.
-Open Scope R_scope.
+From Calculus.Chapter10 Require Import Prelude.
 
 Lemma lemma_10_6_i : ∀ f g f' g' a,
   f = (λ x, g (x + g a)) -> ⟦ der ⟧ f = f' -> ⟦ der ⟧ g = g' -> f' = (λ x, g' (x + g a)).
 Proof.
-  intros f g f' g' a H1 H2 H3. set (h := λ x, x + g a). set (h' := λ _ : ℝ, 1).
-  replace (λ x : ℝ, g (x + g a)) with (g ∘ h) in * by (extensionality y; reflexivity).
-  replace  (λ x : ℝ, g' (x + g a)) with (g' ∘ h) by (extensionality y; reflexivity).
-  assert (H4 : ⟦ der ⟧ h = h').
-  {
-    unfold h, h'. replace (λ _ : ℝ, 1) with (λ x : ℝ, 1 + 0) by (extensionality y; apply Rplus_0_r).
-    apply theorem_10_3_b; [ apply theorem_10_2 | apply theorem_10_1 ].
-  }
-  assert (H5 : ⟦ der ⟧ f = (g' ∘ h) ∙ h') by (rewrite H1; apply chain_rule; auto).
-  replace (g' ∘ h) with (g' ∘ h ∙ h'). 2 : { unfold h'. extensionality y. apply Rmult_1_r. }
-  rewrite (derivative_of_function_unique f f' (g' ∘ h ∙ h') H2 H5). reflexivity.
+  intros f g f' g' a H1 H2 H3.
+  assert (H4 : ⟦ der ⟧ (λ x : ℝ, g (x + g a)) = (λ x : ℝ, g' (x + g a))) by auto_diff.
+  rewrite (derivative_of_function_unique f f' (λ x : ℝ, g' (x + g a)) H2 ltac:(subst; auto)). reflexivity.
 Qed.
 
 Lemma lemma_10_6_ii : ∀ f g f' g' a,
   f = (λ x, g (x * g a)) -> ⟦ der ⟧ f = f' -> ⟦ der ⟧ g = g' -> f' = (λ x, g' (x * g a) * g a).
-Proof. Admitted.
+Proof. 
+  intros f g f' g' a H1 H2 H3.
+  assert (H4 : ⟦ der ⟧ (λ x : ℝ, g (x * g a)) = (λ x : ℝ, g' (x * g a) * g a)) by auto_diff.
+  rewrite (derivative_of_function_unique f f' (λ x : ℝ, g' (x * g a) * g a) H2 ltac:(subst; auto)). reflexivity.
+Qed.
 
 Lemma lemma_10_6_iii : ∀ f g f' g',
   f = (λ x, g (x + g x)) -> ⟦ der ⟧ f = f' -> ⟦ der ⟧ g = g' -> f' = (λ x, g' (x + g x) * (1 + g' x)).
-Proof. Admitted.
+Proof.
+  intros f g f' g' H1 H2 H3.
+  assert (H4 : ⟦ der ⟧ (λ x : ℝ, g (x + g x)) = (λ x : ℝ, g' (x + g x) * (1 + g' x))) by auto_diff.
+  rewrite (derivative_of_function_unique f f' (λ x : ℝ, g' (x + g x) * (1 + g' x)) H2 ltac:(subst; auto)). reflexivity.
+Qed.
 
 Lemma lemma_10_6_iv : ∀ f g f' g' a,
   f = (λ x, g x * (x - a)) -> ⟦ der ⟧ f = f' -> ⟦ der ⟧ g = g' -> f' = (λ x, g' x * (x - a) + g x).
-Proof. Admitted.
+Proof.
+  intros f g f' g' a H1 H2 H3.
+  assert (H4 : ⟦ der ⟧ (λ x : ℝ, g x * (x - a)) = (λ x : ℝ, g' x * (x - a) + g x)) by auto_diff.
+  rewrite (derivative_of_function_unique f f' (λ x : ℝ, g' x * (x - a) + g x) H2 ltac:(subst; auto)). reflexivity.
+Qed.
 
 Lemma lemma_10_6_v : ∀ f g f' a,
   f = (λ x, g a * (x - a)) -> ⟦ der ⟧ f = f' -> f' = (λ x, g a).
-Proof. Admitted.
+Proof.
+  intros f g f' a H1 H2.
+  assert (H3 : ⟦ der ⟧ (λ x : ℝ, g a * (x - a)) = (λ x : ℝ, g a)) by auto_diff.
+  rewrite (derivative_of_function_unique f f' (λ x : ℝ, g a) H2 ltac:(subst; auto)). reflexivity.
+Qed.
 
 Lemma lemma_10_6_vi : ∀ f g f' g',
   f = (λ x, g ((x - 3)^2)) -> ⟦ der ⟧ f = f' -> ⟦ der ⟧ g = g' -> f' = (λ x, g' ((x - 3)^2) * (2 * (x - 3))).
-Proof. Admitted.
+Proof.
+  intros f g f' g' H1 H2 H3.
+  assert (H4 : ⟦ der ⟧ (λ x : ℝ, g ((x - 3)^2)) = (λ x : ℝ, g' ((x - 3)^2) * (2 * (x - 3)))) by auto_diff.
+  rewrite (derivative_of_function_unique f f' (λ x : ℝ, g' ((x - 3)^2) * (2 * (x - 3)) ) H2 ltac:(subst; auto)). reflexivity.
+Qed.
