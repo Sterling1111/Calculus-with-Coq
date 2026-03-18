8e23081822d602c77693bc345af59882ba54cc98
Sterling1111 <sterlingijeppson@gmail.com>
2026-03-17 21:54:26 -0700

    feat: refine lemmas in Chapter 11 by improving assumptions and proofs for inequalities

--- Calculus/Chapter11/Problem_11_28.v
+++ Calculus/Chapter11/Problem_11_28.v
@@ -1,25 +1,57 @@
 From Calculus.Chapter11 Require Import Prelude.
 
-Lemma lemma_11_28_a : forall f f' a b M,
+Lemma lemma_11_28_a : forall a b M f f',
   a < b ->
-  continuous_on f [a, b] ->
-  ⟦ der ⟧ f (a, b) = f' ->
-  (forall x, x ∈ (a, b) -> f' x >= M) ->
+  ⟦ der ⟧ f = f' ->
+  (forall x, x ∈ [a, b] -> f' x >= M) ->
   f b >= f a + M * (b - a).
-Admitted.
+Proof.
+  intros a b M f f' H1 H2 H3.
+  assert (H4 : continuous_on f [a, b]).
+  {
+    apply continuous_imp_continuous_on, differentiable_imp_continuous,
+    (derivative_imp_differentiable f f'); auto.
+  }
+  assert (H5 : ⟦ der ⟧ f (a, b) = f').
+  { apply derivative_imp_derivative_on; auto. apply differentiable_domain_open; auto. }
+  pose proof MVT f f' a b H1 H4 H5 as [x [H6 H7]].
+  specialize (H3 x ltac:(solve_R)).
+  apply Rmult_eq_compat_r with (r := b - a) in H7.
+  field_simplify in H7; nra.
+Qed.
 
-Lemma lemma_11_28_b : forall f f' a b M,
+Lemma lemma_11_28_b : forall a b M f f',
   a < b ->
-  continuous_on f [a, b] ->
-  ⟦ der ⟧ f (a, b) = f' ->
-  (forall x, x ∈ (a, b) -> f' x <= M) ->
+  ⟦ der ⟧ f = f' ->
+  (forall x, x ∈ [a, b] -> f' x <= M) ->
   f b <= f a + M * (b - a).
-Admitted.
+Proof.
+  intros a b M f f' H1 H2 H3.
+  assert (H4 : continuous_on f [a, b]).
+  {
+    apply continuous_imp_continuous_on, differentiable_imp_continuous,
+    (derivative_imp_differentiable f f'); auto.
+  }
+  assert (H5 : ⟦ der ⟧ f (a, b) = f').
+  { apply derivative_imp_derivative_on; auto. apply differentiable_domain_open; auto. }
+  pose proof MVT f f' a b H1 H4 H5 as [x [H6 H7]].
+  specialize (H3 x ltac:(solve_R)).
+  apply Rmult_eq_compat_r with (r := b - a) in H7.
+  field_simplify in H7; nra.
+Qed.
 
-Lemma lemma_11_28_c : forall f f' a b M,
+Lemma lemma_11_28_c : forall a b M f f',
   a < b ->
-  continuous_on f [a, b] ->
-  ⟦ der ⟧ f (a, b) = f' ->
-  (forall x, x ∈ (a, b) -> |f' x| <= M) ->
+  ⟦ der ⟧ f = f' ->
+  (forall x, x ∈ [a, b] -> |f' x| <= M) ->
   |f b - f a| <= M * (b - a).
-Admitted.
+Proof.
+  intros a b M f f' H1 H2 H3.
+  assert (H4 : forall x, x ∈ [a, b] -> f' x >= -M).
+  { intros x H4. specialize (H3 x H4). solve_R. }
+  assert (H5 : forall x, x ∈ [a, b] -> f' x <= M).
+  { intros x H5. specialize (H3 x H5). solve_R. }
+  pose proof lemma_11_28_a a b (-M) f f' H1 H2 H4 as H6.
+  pose proof lemma_11_28_b a b M f f' H1 H2 H5 as H7.
+  solve_R.
+Qed.
--- Calculus/Chapter11/Problem_11_29.v
+++ Calculus/Chapter11/Problem_11_29.v
@@ -1,10 +1,19 @@
 From Calculus.Chapter11 Require Import Prelude.
 
 Lemma lemma_11_29 : forall f f' M,
-  M > 0 ->
-  continuous_on f [0, 1] ->
-  ⟦ der ⟧ f (0, 1) = f' ->
-  (forall x, x ∈ (0, 1) -> f' x >= M) ->
-  exists a b, 0 <= a /\ a < b /\ b <= 1 /\ b - a = 1/4 /\
-  (forall x, x ∈ [a, b] -> |f x| >= M / 4).
+  ⟦ der ⟧ f = f' ->
+  (forall x, x ∈ [0, 1] -> f' x >= M > 0) ->
+  exists a b, b - a = 1/4 /\ forall x, x ∈ [a, b] -> |f x| >= M / 4.
+Proof.
+  intros f f' M H1 H2.
+  assert (H3 : increasing_on f [0, 1]).
+  {
+    apply (derivative_on_pos_imp_increasing_on f f'); try lra.
+    - apply derivative_imp_derivative_on; auto. apply differentiable_domain_closed; lra.
+    - intros x H3. specialize (H2 x H3). lra.
+  }
+  destruct (Rgt_le_dec (f (1/2)) 0) as [H4 | H4].
+  - exists (3/4), 1. split; [ lra | intros x H5 ].
+    specialize (H3 (1/2) (3/4) ltac:(solve_R) ltac:(solve_R) ltac:(lra)).
+    specialize (H2 0 ltac:(solve_R)).
 Admitted.
